c6bccabe2163f27e8a5dcb0a23a046170bab36b1
ZIZizou <atharva.naik@ucalgary.ca>
2021-04-10 17:52:59 +0400

    --added changes to the html files --

--- SMC/table_creator/views.py
+++ SMC/table_creator/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'table_creator/index.html')
+    return render(request, 'table_creator/landing.html')
