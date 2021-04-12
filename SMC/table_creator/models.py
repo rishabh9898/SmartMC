9e137815ca6614ce1d48e92dfd134907658239b9
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-04-12 15:07:44 +0400

    --Final--

--- SMC/table_creator/models.py
+++ SMC/table_creator/models.py
@@ -6,28 +6,43 @@ from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget, Admin
 from embed_video.fields import EmbedVideoField
 
 category_choice = (
-		('Pill Box-1', 'Pill Box-1'),
-		('Pill Box-2', 'Pill Box-2'),
-		
+		('1', '1'),
+		('2','2'),
+		('3','3'),
+		('4','4'),
 	)
 
-class Category(models.Model):
-	name_of_box = models.CharField(max_length=50, blank=True, null=True)
-	def __str__(self):
-		return self.name_of_box
-class SmartMC(models.Model):
-	place_in = models.ForeignKey(Category, on_delete=models.CASCADE)
+category_choice_2 = (
+		('2', '2'),
+	)
+category_choice_3 = (
+		('3', '3'),
+	)
+
+category_choice_4 = (
+		('4', '4'),
+	)
+
+# class ApplicantData(models.Model):
+    # scheduled_at = models.DateTimeField(null=True, blank=True)
+
+
+# class SmartMC(models.Model):
+	# place_in = models.ForeignKey(Category, on_delete=models.CASCADE)
 	
 class SmartMC(models.Model):
-	name = models.CharField(max_length=50, blank=True, null=True)
-	quantity = models.IntegerField(default='0', blank=True, null=True)
-	place_in = models.ForeignKey(Category, on_delete=models.CASCADE,blank=True)
-	receive_by = models.CharField(max_length=50, blank=True, null=True)
-	phone_number = models.CharField(max_length=50, blank=True, null=True)
-	date=models.DateField(default=timezone.now)
+	name = models.CharField("Please Enter your Full Name",max_length=50, blank=True, null=True)
+	pill_Box1 = models.CharField("Add the Box you want to store pill in ",max_length=50, blank=True, null=True,choices=category_choice)
+	total_quantity=models.IntegerField(" Total Amount of Pills ",default='0', blank=True, null=True)
+	quantity = models.IntegerField("Pills you wish to take now",default='0', blank=True, null=True)
+	
+	# pill_Box2 = models.CharField(max_length=50, blank=True, null=True,choices=category_choice)
+	# pill_Box3 = models.CharField(max_length=50, blank=True, null=True,choices=category_choice)
+	# pill_Box4 = models.CharField(max_length=50, blank=True, null=True,choices=category_choice)
 	date_now=models.DateTimeField(auto_now=True)
+	day_name = models.CharField("Please Enter the name of the day",max_length=50, blank=True, null=True)
 	email=models.EmailField(max_length=200)
-	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+	
 	# export_to_CSV = models.BooleanField(default=False)
 
 	def __str__(self):
