d46f04a82e96a70259e00487eed2ac20bc4599ef
ZIZizou <atharva.naik@ucalgary.ca>
2021-04-12 12:36:47 +0400

    --minor changes--

--- SMC/table_creator/models.py
+++ SMC/table_creator/models.py
@@ -15,8 +15,7 @@ class Category(models.Model):
 	name_of_box = models.CharField(max_length=50, blank=True, null=True)
 	def __str__(self):
 		return self.name_of_box
-class SmartMC(models.Model):
-	place_in = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 	
 class SmartMC(models.Model):
 	name = models.CharField(max_length=50, blank=True, null=True)
